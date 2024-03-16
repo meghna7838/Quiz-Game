abe219f3bc8a623ceb3d01a818d8c5f43b4f6f76
meghna7838 <meghnatandon7838@gmail.com>
2024-03-16 17:21:18 +0530

    Adding options and score

--- quiz.js
+++ quiz.js
@@ -3,17 +3,19 @@ const questionObj =
       category: 'Food & Drink',
       id: 'qa-1',
       correctAnswer: 'Three',
-      options: ['Two', 'Three ', 'Four', 'Five'],
+      options: ['Two', 'Three', 'Four ', 'Five'],
       question:
         "How many pieces of bun are in a Mcdonald's Big Mac?",
     };
 
+    let score=0;
+    
     //Destructuring of the obj as we will be needing it repeatedly
     const {
       correctAnswer,
       options,
       question
-    } = questionObj
+    } = questionObj;
     
     //Getting element from html and filling data here
     const questionEl = document.getElementById("question");
@@ -23,3 +25,25 @@ const questionObj =
     questionEl.innerHTML = question;
     //We can also use text content here instead of html.
 
+    options.forEach((opt) => {
+      const btn = document.createElement("button");
+      btn.textContent = opt;
+      optionEl.appendChild(btn);
+
+      //Event handling on btn
+      
+      btn.addEventListener('click',()=>{
+        if(opt===correctAnswer)
+        {
+          score++;
+          //console.log("I am correct");
+        }else{
+          score = score-0.25;
+        }
+        //console.log(score);
+        scoreEl.textContent = `Score: ${score}`;
+        questionEl.textContent = `Quiz completed!!!`;
+        optionEl.textContent = "";
+      });
+    });
+    
